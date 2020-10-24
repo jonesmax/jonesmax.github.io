2569c579427cb14159e7c1122465d7c2649a84d6
jonesmax <maxjones2001@hotmail.com>
2020-10-24 16:58:52 -0400

    update modals

--- src/components/home.vue
+++ src/components/home.vue
@@ -4,14 +4,23 @@
     <div id="content">
         <div id="topBar">
             <h1>Maxwell Jones</h1>
-            
                 <li  v-for="item of itemDirectory" v-bind:key="item['.key']" :style="getBackgroundColor(item) " v-on:click="changeActive(itemDirectory,item) ">
                     {{item.name}}
                 </li>
-
-
+        </div>
+        <div id="modalMe"  v-if="itemDirectory.me.active == true">
+            <h1>About Me</h1>
+        </div>
+        <div id="modalSkills"  v-if="itemDirectory.skills.active == true">
+            <h1>My Skills</h1>
+        </div>
+        <div id="modalPro"  v-if="itemDirectory.projects.active == true">
+            <h1>Projects</h1>
         </div>
     </div>
+    
+    
+
 
   </div>
 </template>
@@ -21,25 +30,24 @@
 export default {
     data(){
         return{
-            itemDirectory: [
-                {
+            itemDirectory: {
+                me: {
                     name: "Me",
                     active: true
                 },
-                {
+                skills: {
                     name: "Skills",
                     active: false
                 },
-                {
+                projects: {
                     name: "Projects",
                     active: false
                 },
-            ]
+            }
         }
     },
     methods:{
         getBackgroundColor(item){
-            
             if(item.active == true){
                 return "background-color: rgb(220, 220, 220)";
             }
@@ -48,7 +56,6 @@ export default {
             for (var key in full) {
                 full[key]["active"] = false;
             }
-            console.log(full);
             item.active = true;
 
             return full,item;
@@ -61,6 +68,7 @@ export default {
 
 #app {
     margin-top: 150px;
+    user-select: none;
 }
 #content{
     background-color: rgb(255, 255, 255);
@@ -68,16 +76,17 @@ export default {
     width: 55%;
     margin: auto;
     border-radius: 10px;
+    box-shadow: 5px 8px #5f5f5f;
 }
 #topBar{
-    background-color: rgb(204, 204, 204);
+    background-color: rgb(255, 255, 255);
     width: 100%;
     height: 70px;
     border-radius: 10px 10px 0px 0px;
     font-family: Arial, Helvetica, sans-serif;
     float: left;
 }
-#topBar h1{
+h1{
     margin: 0;
     padding-left: 15px;
     padding-top: 1.5%;
@@ -98,6 +107,19 @@ export default {
     background-color: rgb(185, 185, 185);
     cursor: pointer;
 }
+#modalMe,#modalSkills,#modalPro{
+    background-color: rgb(226, 226, 226);
+    height: 600px;
+    width: 100%;
+    border-radius: 10px;
+}
+#modalMe h1, #modalSkills h1,#modalPro h1{
+    font-size: 20px;
+    font-family: Arial, Helvetica, sans-serif;
+    
+}
+
+
 
 
 </style>
