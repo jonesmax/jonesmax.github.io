00614eaf4972e21416066fca8bf7ed4c79466e6b
jonesmax <maxjones2001@hotmail.com>
2020-10-25 00:44:53 -0400

    design

--- src/components/home.vue
+++ src/components/home.vue
@@ -11,9 +11,11 @@
     <div id="content">
         <div id="topBar">
             <h1>Maxwell Jones</h1>
+            <img src="../images/moving.gif">
                 <li  v-for="item of itemDirectory" v-bind:key="item['.key']" :style="getBackgroundColor(item) " v-on:click="changeActive(itemDirectory,item) ">
                     {{item.name}}
                 </li>
+                
         </div>
         <div id="modalMe"  v-if="itemDirectory.me.active == true">
             <div id="software">
@@ -30,6 +32,7 @@
         </div>
         <div id="modalSkills"  v-if="itemDirectory.skills.active == true">
             <h1>My Skills</h1>
+            
         </div>
         <div id="modalPro"  v-if="itemDirectory.projects.active == true">
             <h1>Projects</h1>
@@ -62,7 +65,7 @@ export default {
     methods:{
         getBackgroundColor(item){
             if(item.active == true){
-                return "background-color: rgb(230, 230, 230)";
+                return "background-color: rgb(220, 220, 220)";
             }
         },
         changeActive(full,item){
@@ -81,6 +84,13 @@ export default {
     font-family: 'Trispace', sans-serif;
     user-select: none;
 }
+img{
+    width: 6%;
+    float: right;
+    margin-top: 0px;
+    margin-right: 6px;
+    border-radius: 10px 40px 0px 0px;
+}
 button{
     margin-top: 0px!important;
     background-color: #4fafcc; /* Green */
@@ -90,7 +100,7 @@ button{
     text-align: center;
     text-decoration: none;
     display: inline-block;
-    font-size: 14px;
+    font-size: 90%;
     margin-left: 20px;
     border-radius: 4px;
     font-family: 'Trispace', sans-serif;
@@ -116,8 +126,8 @@ button:hover{
 }
 #topBar{
     background-color: rgb(255, 255, 255);
-    width: 100%;
-    height: 77px;
+    width: 100;
+    height: 8.2vh;
     border-radius: 10px 10px 0px 0px;
     font-family: 'Trispace', sans-serif;
     float: left;
@@ -131,13 +141,14 @@ button:hover{
     padding-bottom: 1%;
     padding-right: 2.5%;
     float: left;
+    font-size: 4vh;
 }
 #topBar li{
     float: left;
     list-style-type:none;
     margin-top: 1.3%;
     margin-right: 3%;
-    font-size: 22px;
+    font-size: 2.3vh;
     padding: 1.3%;
     border-radius: 15px;
 }
@@ -147,7 +158,7 @@ button:hover{
 }
 #modalMe,#modalSkills,#modalPro{
     background-color: rgb(226, 226, 226);
-    height: 550px;
+    height: 60vh;
     width: 100%;
     border-radius: 10px;
 }
@@ -162,7 +173,7 @@ button:hover{
 #software{
     width: 50%;
     border-radius: 10px;
-    background-color: rgb(168, 84, 84);
+    /*background-color: rgb(168, 84, 84);*/
     height: 550px;
 }
 #software h1,h3{
